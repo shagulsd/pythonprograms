4ab56e69d1771202fdfbf4f8d8f957caa8618f22
shagulsd <shagu1719220@gmail.com>
2022-01-26 19:15:54 -0800

    basics

--- test.py
+++ test.py
@@ -1,2 +1,54 @@
 print("hello")
 print("shagul")
+
+reversed numbers
+number=1234
+rev=0
+while(number>0):
+    rem=number%10
+    rev=rev*10+rem
+    number=number//10
+print(rev)
+
+# leap year
+year=int (input("enter the year"))
+if(year % 400 == 0 and year % 100 == 0):
+    print(year,"leap year")
+else:
+    print(year,"not a leap year")
+
+
+# prime number
+number= int(input("enter number"))
+count=0
+
+for i in range(1,number+1):
+    if (number % i) == 0:
+        count+= 1
+if count== 2:
+    print('prime number')
+else:
+    print('not a prime number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
