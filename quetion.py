3e03e205a513dde8518a350c17e86a69abd9fd36
shagulsd <shagu1719220@gmail.com>
2022-02-03 07:32:55 -0800

    class funtions

---
+++ quetion.py
@@ -0,0 +1,23 @@
+
+
+class don ():
+    def shagul (self ,c,g,h):
+        return c+g+h
+    def sathish (self,a,b,c):
+        return a+b+c
+yy=  don ()
+print(yy.shagul(1,2,3))
+# print(yy.sathish(3,4,6))
+
+
+
+
+
+
+
+
+
+
+
+
+
